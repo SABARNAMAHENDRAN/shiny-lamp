253d9d43cba00b0d2a67cc25b1f12319c3451982
sabarna Mahendran <sabarnamahendran@gmail.com>
2025-02-05 15:43:39 +0530

    ver1

---
+++ Untitled_1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    scanf("enter the value of a and b:%d%d");
+    printf("a+b:%d");
+    return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a = 10;
+    float b = 10.0455;
+    char c = 's';
+    char name[10] = "sabarna";
+
+    printf("the value of varaible a : %d \n",a);
+    printf("the value of varaible b : %.1f \n",b);
+    printf("the value of varaible c : %c \n",c);
+    printf("the value of varaible c : %s \n",name);
+    return 0; 
+}
